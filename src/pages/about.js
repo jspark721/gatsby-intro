249b0870d181ee2e5bcb9153173b831d9b220e12
jspark721 <jspark721@gmail.com>
2019-11-25 15:27:20 -0800

    page set up

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default() => (
+    <>
+        <h1>About Page</h1>
+        <p>this is the about page for the personal website</p>
+        <Link to="/">&larr; back to home</Link>
+    </>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => <>
+    <h1>Home</h1>
+    <p>Hi Frontend Masters!</p>
+    <Link to="/about">learn about me &rarr;</Link>
+    </>
+ 

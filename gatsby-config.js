f60a39198c957c4a1a2c8b0a03322eb345675c09
jspark721 <jspark721@gmail.com>
2019-11-27 12:18:01 -0800

    add contact page and gatsby-mdx-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
         title: 'Gatsby Intro Workshop',
         description: 'A site built from scratch with Gatsby, React, and Netlify',
     },
-    plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+    plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet',
+    {
+        resolve: 'gatsby-plugin-mdx',
+        options: {
+            defaultLayouts: {
+                default: require.resolve('./src/components/layout.js'),
+            }
+        }
+    }
+],
 }
